e8ed9ad4338d9766e8fea6a346645ef97c996abb
Br0k3nSt4r <diegoromero1@colegioguanenta.edu.co>
2026-02-19 16:49:57 -0500

    Se actualizo el Repositorio General con Subcarpetas y todo

---
+++ rock_paper_scissors.py
@@ -0,0 +1,53 @@
+#emojies backup ✊​✋​✌️​
+import random
+
+print("Piedra Papel o Tijeras")
+
+#input
+print()
+print("Elige:")
+print("1. Piedra")
+print("2. Papel")
+print("3. Tijeras")
+
+User = int(input())
+
+Machine = random.randint(1,3)
+
+#procesing
+print("Resultados")
+if User == 1:
+    if Machine == 1:
+        print("✊​ / ✊​")
+        print("Empate")
+    elif Machine == 2:
+        print("✊​ / ✋​")
+        print("Perdiste")
+    else:
+        print("✊ / ✌️​")
+        print("Ganaste")
+elif User == 2:
+    if Machine == 1:
+        print("✋ / ✊​​")
+        print("Ganaste")
+    elif Machine == 2:
+        print("✋ / ✋​")
+        print("Empate")
+    else:
+        print("✋ / ✌️​")
+        print("Perdiste")
+elif User == 3:
+    if Machine == 1:
+        print("✌️​ / ✊​")
+        print("Perdiste")
+    elif Machine == 2:
+        print("✌️ / ✋​​")
+        print("Ganaste")
+    else:
+        print("✌️ / ✌️​​")
+        print("Empate")
+else:
+    print("ERROR!: Elección no valida")
+    exit()
+#output
+print("---------------------------------")
